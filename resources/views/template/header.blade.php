dc532bab8590a14dc1a6360eb1c0e1c074d9757b
Marcell-039 <marcellinoknl@gmail.com>
2022-04-11 23:24:41 +0700

    laravel in project

---
+++ resources/views/template/header.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Wisata Kawasan Danau Toba</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Arizonia&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="css/animate.css">
+	
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	<link rel="stylesheet" href="css/jquery.timepicker.css">
+
+	
+	<link rel="stylesheet" href="css/flaticon.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
+		<div class="container">
+			<a class="navbar-brand" href="/">Danau Toba<span>Travel & Tourism</span></a>
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="oi oi-menu"></span> Menu
+			</button>
+
+			<div class="collapse navbar-collapse" id="ftco-nav">
+				<ul class="navbar-nav ml-auto">
+					<li class="nav-item active"><a href="index.html" class="nav-link">Beranda</a></li>
+					<li class="nav-item"><a href="about.html" class="nav-link">Desa Wisata</a></li>
+                    <div class="navbar-nav ml-auto dropdown show">
+                        <li class="nav-item" type="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <a href="/blog" class="nav-link">Blog</a>
+                        </li>
+                        <div class="dropdown-menu " aria-labelledby="dropdownMenuLink">
+                          <button class="dropdown-item" type="button">Atraksi</button>
+                          <button class="dropdown-item" type="button">Event</button>
+                          <button class="dropdown-item" type="button">Berita</button>
+                        </div>
+                      </div>
+					<li class="nav-item"><a href="blog.html" class="nav-link">Fasilitas</a></li>
+					<li class="nav-item"><a href="contact.html" class="nav-link">Berita</a></li>
+                    <li class="nav-item"><a href="contact.html" class="nav-link" ><i class="fa fa-sign-in" aria-hidden="true"></i> Masuk</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<!-- END nav -->
