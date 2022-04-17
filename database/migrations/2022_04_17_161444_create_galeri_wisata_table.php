5464ad88cd48fa22e6693c09ea4f4ead223e6462
Marcell-039 <marcellinoknl@gmail.com>
2022-04-17 23:16:07 +0700

    data db

---
+++ database/migrations/2022_04_17_161444_create_galeri_wisata_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGaleriWisataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('galeri_wisata', function (Blueprint $table) {
+            $table->id('id_galeri');
+            $table->string('file_foto');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('galeri_wisata');
+    }
+}
