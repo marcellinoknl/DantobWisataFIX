2f4980fc730091c7817caed3b2dbbd151abf1f30
Marcell-039 <marcellinoknl@gmail.com>
2022-04-27 08:35:07 +0700

    front end

--- resources/views/admin/tambah-objek-wisata.blade.php
+++ resources/views/admin/tambah-objek-wisata.blade.php
@@ -77,7 +77,7 @@
                                             <label class="col-sm-3 col-form-label">Deskripsi Wisata</label>
                                         </div>
                                         <div class ="mb-3 row">
-                                            <textarea type="text" required="required" id="deskripsiwisata" name="deskripsi" height="300px" class="form-control" ></textarea>
+                                            <textarea required="required" id="deskripsiwisata" name="deskripsi" cols="200px" rows="20" ></textarea>
                                         </div>
                                         <br><br>    
                                         <button type="button" class="btn btn-danger" onclick="window.location.href='/kelolaobjek'"><i class="ti-close"></i> Batal</button>
