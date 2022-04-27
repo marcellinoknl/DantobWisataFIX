b1a49f1b93332b93fa7cb3997ce0252c4dc80abf
Samuel Simanjuntak <samuelsimanjuntak194@gmail.com>
2022-04-27 10:31:09 +0700

    seeder

--- database/seeders/KabupatenSeeder.php
+++ database/seeders/KabupatenSeeder.php
@@ -13,6 +13,39 @@ class KabupatenSeeder extends Seeder
      */
     public function run()
     {
-        //
+        \App\Models\Kabupaten::insert([
+            [
+                'nama_kab' => 'Samosir',
+                'file_foto' => 'samosir.jpeg'
+            ],
+            [
+                'nama_kab' => 'Toba',
+                'file_foto' => 'Toba.jpg'
+            ],
+            [
+                'nama_kab' => 'Dairi',
+                'file_foto' => 'dairi.jpg'
+            ],
+            [
+                'nama_kab' => 'Simalungun',
+                'file_foto' => 'Simalungun.jpg'
+            ],
+            [
+                'nama_kab' => 'Humbang Hasundutan',
+                'file_foto' => 'Humbang.jpg'
+            ],
+            [
+                'nama_kab' => 'Pakpak Bharat',
+                'file_foto' => 'pakpak.jpg'
+            ],
+            [
+                'nama_kab' => 'Tapanuli Utara',
+                'file_foto' => 'taput.jpg'
+            ],
+            [
+                'nama_kab' => 'karo',
+                'file_foto' => 'karo.jpg'
+            ]
+        ]);
     }
 }
