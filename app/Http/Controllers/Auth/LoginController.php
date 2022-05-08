cca37fe71d7cc1e08006c9d4d1ac3296ffa640b0
Samuel Simanjuntak <samuelsimanjuntak194@gmail.com>
2022-05-08 22:50:38 +0700

    NewAdding

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
@@ -26,7 +27,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo ='/';
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
@@ -36,5 +37,29 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function login(Request $request)
+    {
+        $input = $request->all();
+
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (auth()->attempt(array('email' => $input['email'], 'password' =>
+        $input['password']))) {
+            if (auth()->user()->role == 1) {
+                return redirect('/admin-wisata')->with('success', "Berhasil login!");
+            } else if (auth()->user()->role == 2) {
+                return redirect('/admin-wisata')->with('success', "Berhasil login!");
+            } else if (auth()->user()->role == 0) {
+                return redirect('/')->with('success', "Berhasil login!");
+            }
+        } else {
+            return redirect()->route('login')
+                ->with('login', 'Email-Address atau Password salah ! ');
+        }
     }
 }
