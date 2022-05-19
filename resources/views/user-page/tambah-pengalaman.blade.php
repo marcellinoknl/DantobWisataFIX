5fb79dca85c2c0d1ccc78952c36480822a7e2c1c
Marcell-039 <marcellinoknl@gmail.com>
2022-05-19 22:10:08 +0700

    data

--- resources/views/user-page/tambah-pengalaman.blade.php
+++ resources/views/user-page/tambah-pengalaman.blade.php
@@ -23,29 +23,10 @@
         
         <div class="container p-md-2 p-2">
             <form>
-                <div class="form-row">
-                  <div class="form-group col-md-6">
-                    <label for="inputEmail4">Email</label>
-                    <input type="email" class="form-control" id="inputEmail4" placeholder="Email">
-                  </div>
-                  <div class="form-group col-md-6">
-                    <label for="inputPassword4">Password</label>
-                    <input type="password" class="form-control" id="inputPassword4" placeholder="Password">
-                  </div>
-                </div>
                 <div class="form-group">
-                  <label for="inputAddress">Address</label>
-                  <input type="text" class="form-control" id="inputAddress" placeholder="1234 Main St">
+                  <label for="inputAddress">Judul Pengalaman Wisata</label>
+                  <input type="text" class="form-control" id="inputAddress" placeholder="Masukkan Pengalaman Anda">
                 </div>
-                <div class="form-group">
-                  <label for="inputAddress2">Address 2</label>
-                  <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
-                </div>
-                <div class="form-row">
-                  <div class="form-group col-md-6">
-                    <label for="inputCity">City</label>
-                    <input type="text" class="form-control" id="inputCity">
-                  </div>
                   <div class="form-group col-md-4">
                     <label for="inputState">State</label>
                     <select id="inputState" class="form-control">
